3e1abb19ee9a5df4be14e346d12b22dbb3d48cb2
Ulnyrova Ekaterina SGC <eulnyrova@dpd.ru>
2022-10-26 21:39:49 +0300

    done #3

--- task_3/solution.js
+++ task_3/solution.js
@@ -17,15 +17,15 @@ function renderCartItem(item) {
     // 1. Создайте переменную itemCountText, равную пустой строке
     let itemCountText = '';
     // 2. Присвойте переменной itemCountText результат склеивания itemCountText и количества
-    itemCountText += count;
+    itemCountText += product.count;
     // 3. Присвойте переменной itemCountText результат склеивания itemCountText и ` × `
     itemCountText += ` × `;
     // 4. Присвойте переменной itemCountText результат склеивания itemCountText и цены
-    itemCountText += price;
+    itemCountText += product.price;
     // 5. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽ = `
     itemCountText += ` ₽ = `;
     // 6. Создайте переменную sum и присвойте ей значение произведения количества и цены
-    let sum = count * price
+    let sum = product.count * product.price
     // 7. Присвойте переменной itemCountText результат склеивания itemCountText и sum
     itemCountText += sum;
     // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
