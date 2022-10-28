9f294d424469530a6456b6cebd265c32d1f637e5
Ulnyrova Ekaterina SGC <eulnyrova@dpd.ru>
2022-10-28 18:09:25 +0300

    final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -37,17 +37,17 @@ function sendRequest(name, phone, address, goods, sum) {
     let data = {goods: [], order: {}};
     
     data.client = `${name} ${phone}`;
-    
-    data.order.address = `${address.street}, ${address.house}, ${address.entrance}, ${address.floor}, ${address.flat}`;
+      
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
     data.order.sum = sum;
     
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
+    for (let i = 0; i < countOfGoods; i += 1) {
         data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
